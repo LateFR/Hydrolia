4cd25a41bd9d7c587f2e519291224c66c0f97fac
LateFR <rigo.lucas@icloud.com>
2025-02-12 19:48:48 +0100

    Update

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 import MainScene from "/scenes/MainScene.js";
 
 let config={
-    type:Phaser.AUTO,
-    with:800,
-    height:600,
-    scenes: [MainScene],
-    physics:{
+    type: Phaser.AUTO, //Phaser choisit automatiquement si il faut utiliser Canva ou WebGL
+
+    scale: {
+        mode: Phaser.Scale.RESIZE, //redimensionne le jeu automatiquement
+        auto: Phaser.Scale.CENTER_BOTH //centre automatiquement le jeu
+    },
+    with: window.innerWidth, //prend toute la largeur de l'écran
+    height: window.innerHeight, //prend toute la hauteur de l'écran
+
+    scenes: [MainScene], //Ajouter chaque scene dans ce tableau
+    physics: {
         default:"arcade",
         arcade:{debug:true}
-    }
+    },
+    pixelArt: true, // Évite le flou sur les jeux en pixel art
 }
 
 let game= new Phaser.Game(config)
--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -6,9 +6,11 @@ export default class MainScene extends Phaser.Scene{
 
     }
     create(){
-        let player = this.physics.add.sprite(100,100,"player") // créé un nouveau sprite avec pour id "player". physics le rend sensible à la physique. On peut faire un sprite sans "physics"
+        const player = this.add.rectangle(50,50,50,50,0xffffff) // créé un nouveau sprite avec pour id "player". physics le rend sensible à la physique. On peut faire un sprite sans "physics". Ici, tant qu'on a pas d'Asset, c'est un rectagle. Ligne originale: this.physics.add.sprite(100,100,"player")
+        this.physics.add.existing(player)//ajoute la physique. A supprimer quand sprite et assets
+        player.body.setCollideWorldBounds(true); // Empêche de sortir de l'écran 
 
-        let platform = this.physics.add.staticGroup() //groupe d'objet statique. Plateforme dans notre cas
+        const platform = this.physics.add.staticGroup() //groupe d'objet statique. Plateforme dans notre cas
         platform.create(400,500,"platform") 
 
         this.physics.add.collider(player,platform)
