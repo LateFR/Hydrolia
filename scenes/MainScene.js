8b944fc49436dd3f8927899a0851e926b7982b08
LateFR <rigo.lucas@icloud.com>
2025-02-12 23:20:32 +0100

    Fin de la création de la fonction de saut
    Quelques modifications mineures également (head du title, suppression de variable inutiles,...)
    La fonction ne fonctionne pas bien et doit être améliorée

--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -6,8 +6,7 @@ export default class MainScene extends Phaser.Scene{
         this.cursors
         this.isJumping = false //pour savoir si on est en train de sauter
         this.defaultGravityY = 500 //valeur de gravité Y par défaut. Ces 2 variables permettent de revenir a la gravité "normal" après une modification de la gravité
-        this.defaultGravityX = 500 //valeur de graité X par défaut
-        this.onTheGround = false //pour savoir si le joueur touche le sol
+        this.defaultGravityX = 0 //valeur de graité X par défaut
     }
     preload(){  // Fonction où charger nos assets
         this.load.image("player","/assets/black_square.png") //charge notre image de player (un carré noir pour l'instant)
@@ -16,13 +15,13 @@ export default class MainScene extends Phaser.Scene{
         this.cameras.main.setBackgroundColor(0xffffff) //set le backround en balnc (hex)
 
         this.player = this.physics.add.sprite(this.game.config.width/2,this.game.config.height/2,"player") // créé un nouveau sprite avec pour id "player". physics le rend sensible à la physique. On peut faire un sprite sans "physics". this.game.config.width/x permet de definir la position du sprite par rapport a la taille de la fentre et donc de le placer au centre
-        this.player.setGravity(0,0)
+        this.player.setGravity(this.defaultGravityX,this.defaultGravityY) //definit la gravité du joueur (que du joueur!!)
         this.player.setDisplaySize(this.game.config.width/100,this.game.config.height/100) //this.game.config.width/x permet de definir la taille du sprite par rapport a la taille de la fentre et donc de conserver le responsive
 
         this.player.body.setCollideWorldBounds(true); // Empêche de sortir de l'écran 
 
         this.platform = this.physics.add.staticGroup() //groupe d'objet statique. Plateforme dans notre cas
-        this.platform.create(1,1,"platform") 
+        this.platform.create(this.game.config.width/2,this.game.config.height,"assets/black_square.png") //on le place au sol au millieu
 
         this.physics.add.collider(this.player,this.platform)
         this.cursors = this.input.keyboard.createCursorKeys()
@@ -49,13 +48,9 @@ export default class MainScene extends Phaser.Scene{
         if(this.cursors.space.isDown){
             this.jump()
         }
-        if (this.player.body.touching.down) {//surveille si le joueur touche le sol
-            this.onTheGround = true; // Le joueur est sur le sol
-        }else{
-            this.onTheGround = false // Le joueur ne touche pas le sol
-        }
     }
-    jump(y=200,speed=200){ //fonction de saut. Y designe la hauteur du saut et Speed la vitesse. Par défaut, les 2 sont à 200
+    //Je pense que cette fonction est facilement optimisable. Merci de le faire dès que possible
+    jump(y=200,speed=400){ //fonction de saut. Y designe la hauteur du saut et Speed la vitesse. Par défaut, les 2 sont à 200
         if (this.isJumping){ //on verifie si on ne saute pas deja. Si oui, on stop
             return "Already jumping"
         }
@@ -65,8 +60,17 @@ export default class MainScene extends Phaser.Scene{
         
         this.time.delayedCall(y, ()=>{ //on attend y temps la montée.
             this.player.setVelocityY(0) //on stop la montée
-            this.player.setGravityY(this.defaultGravityY) //on remet la gravité par défaut
+            this.player.setGravityY(this.defaultGravityY) //on remet la gravité par défaut //on attend que le joueur touche le sol
+            this.waitHitGround(()=>{this.isJumping = false}) //fin du saut lorsqu'on touche le sol
         })
-        this.isJumping = false //on dit qu'on a fini le saut
     }
+    waitHitGround(callback){ //fonction auxiliaire à jump. Attend que this.player.body.touching.down==true
+        let interval = setInterval(() => {
+            if (this.player.body.touching.down) {
+                clearInterval(interval); // Arrête la vérification
+                callback(); // Exécute la suite du code
+            }
+        }, 200); // Vérifie la condition toutes les 200ms
+    }
+
 }
