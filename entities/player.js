aa7004d7744d15dfa9e932b49adf80247ce6ab53
LateFR <rigo.lucas@icloud.com>
2025-02-13 23:36:41 +0100

    Améliorations
    Des listeners de Player et ajout d'un suivit de caméra du player

--- entities/player.js
+++ entities/player.js
@@ -5,11 +5,14 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
 
         this.scene = scene
         this.cursors
-        this.isJumping = false //pour savoir si on est en train de sauter
+
         this.defaultGravityY = 500 //valeur de gravité Y par défaut. Ces 2 variables permettent de revenir a la gravité "normal" après une modification de la gravité
         this.defaultGravityX = 0 //valeur de graité X par défaut
         this.playerSpeed = 200 //vitesse du joueur. Peut importe le sens
-
+        
+        this.isJumping = false //pour savoir si on est en train de sauter
+        this.isDashing = false //pour savoir si on est en train de dasher
+        this.pressed=  false //définit si une touche a été pressee sur la frame
        
     }
     create(){
@@ -17,39 +20,52 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
         this.scene.physics.add.existing(this)
         
         this.setDisplaySize(this.scene.game.config.width/100,this.scene.game.config.height/100) //this.scene.game.config.width/x permet de definir la taille du sprite par rapport a la taille de la fentre et donc de conserver le responsive
-
+        this.setGravity(this.defaultGravityX,this.defaultGravityY) //definit la gravité du joueur (que du joueur!!)
         this.setCollideWorldBounds(true); // Empêche de sortir de l'écran 
 
-        this.cursors = this.scene.input.keyboard.createCursorKeys()
+        this.setupListeners()
     }
     update(){
-        let pressed=false
-        if (this.cursors.left.isDown){ //si fleche gauche pressé
-            this.setVelocityX(-this.playerSpeed) //on met une velocité de 100 sur l'axe X
-            pressed=true //on verrouille pour ne pas que le dernier if stop la velocité
-        }
-        if (this.cursors.right.isDown){ //si fleche droite
-            this.setVelocityX(this.playerSpeed) //on va vers la droite
-            pressed=true
-        }
-        if(this.cursors.space.isDown){
-            this.jump()
-        }else if(!pressed){
+        
+        if(!pressed){
             this.setVelocityX(0)
+            this.pressed==false
         }
         if (this.body.blocked.down) { //on verifit si on touche le sol. Si oui, on dit que le saut est stoppé
             this.isJumping=false 
         }
     }
 
-    jump(y=500){ //fonction de saut. Y designe la hauteur du saut
-        
-        if (this.isJumping || !this.body.blocked.down) return; // Empêche le double saut si le joueur n'est pas au sol
-        this.isJumping = true //si on ne saute pas déja, on dit qu'on saute a present
-        this.setGravityY(0)
-        this.setVelocityY(y)
-        this.setGravityY(this.defaultGravityY)
+    setupListeners(){//fonction d'initialisation réunnisant tout les listeners
+        this.scene.input.keyboard.on("keydown-LEFT",(event)=>{
+            this.setVelocityX(-this.playerSpeed) //on met une velocité de 100 sur l'axe X
+            this.pressed=true //on verrouille pour ne pas que le dernier if stop la velocité
+        })
+        this.scene.input.keyboard.on("keydown-RIGHT",(event)=>{
+            this.setVelocityX(this.playerSpeed) //on va vers la droite
+            this.pressed=true
+        })
+        this.scene.input.keyboard.on("keydown-SPACE",(event)=>{
+            this.jump()
+        })
+        this.scene.input.keyboard.on("Keydown-KeyE",(event)=>{
+            this.dash()
+        })
 
+    }
+    jump(y=500){ //fonction de saut. Y designe la hauteur du saut
+
+        if (this.isJumping || !this.body.blocked.down) return; // Empêche le double saut si le joueur n'est pas au sol
+
+        this.isJumping = true //si on ne saute pas déja, on dit qu'on saute a present
+        this.setVelocityY(-y)
+
+    }
+
+    dash(x=500){
+        if (this.isDashing) return
+        
+        this.setAccelerationX(x)
     }
 
 }
--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -12,7 +12,10 @@ export default class MainScene extends Phaser.Scene{
     create(){
         this.cameras.main.setBackgroundColor(0xffffff) //set le backround en balnc (hex)
 
+        this.physics.world.setBounds();// Désactive la limite du monde (donc il faut faire attention a pas faire de lag)
+
         this.player = new Player(this,100,100)
+        this.cameras.main.startFollow(this.player, true, 0.1, 0.1); //La caméra suit le joueur
         this.player.create(); // on appelle le create
         this.add.existing(this.player);
         this.physics.add.existing(this.player);
