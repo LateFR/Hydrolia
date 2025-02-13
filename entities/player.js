f1050f9a9773ca0c4a24eef0bec899f87030c028
LateFR <rigo.lucas@icloud.com>
2025-02-14 00:23:51 +0100

    Création du dash de player
    Et correction de bugs multiples

--- entities/player.js
+++ entities/player.js
@@ -8,12 +8,13 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
 
         this.defaultGravityY = 500 //valeur de gravité Y par défaut. Ces 2 variables permettent de revenir a la gravité "normal" après une modification de la gravité
         this.defaultGravityX = 0 //valeur de graité X par défaut
-        this.playerSpeed = 200 //vitesse du joueur. Peut importe le sens
+        this.playerSpeed = 200 //vitesse du joueur. Peut importe la direction
         
         this.isJumping = false //pour savoir si on est en train de sauter
         this.isDashing = false //pour savoir si on est en train de dasher
+        this.okEndDashing = false //variable intermediare pour donner la fin du dash et la fin de l'appuit sur la touche E pour empecher le spam
         this.pressed=  false //définit si une touche a été pressee sur la frame
-       
+        this.direction = "right" //donne la direction du player. "left" ou "right" pour l'instant
     }
     create(){
         this.scene.add.existing(this) //on ajoute le player (this) à la scène et au jeu
@@ -27,9 +28,8 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
     }
     update(){
         
-        if(!pressed){
-            this.setVelocityX(0)
-            this.pressed==false
+        if (!this.pressed && !this.isDashing) {
+            this.setVelocityX(0);
         }
         if (this.body.blocked.down) { //on verifit si on touche le sol. Si oui, on dit que le saut est stoppé
             this.isJumping=false 
@@ -40,15 +40,29 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
         this.scene.input.keyboard.on("keydown-LEFT",(event)=>{
             this.setVelocityX(-this.playerSpeed) //on met une velocité de 100 sur l'axe X
             this.pressed=true //on verrouille pour ne pas que le dernier if stop la velocité
+            this.direction = "left"
+            this.setFlipX(true) // Retourne le sprite vers la gauche
         })
         this.scene.input.keyboard.on("keydown-RIGHT",(event)=>{
             this.setVelocityX(this.playerSpeed) //on va vers la droite
             this.pressed=true
+            this.direction = "right"
+            this.setFlipX(false) // Retourne le sprite vers la droite
         })
+
+        this.scene.input.keyboard.on("keyup-LEFT", (event) => {
+            this.pressed = false;
+        });
+
+        this.scene.input.keyboard.on("keyup-RIGHT", (event) => {
+            this.pressed = false;
+        });
+
+
         this.scene.input.keyboard.on("keydown-SPACE",(event)=>{
             this.jump()
         })
-        this.scene.input.keyboard.on("Keydown-KeyE",(event)=>{
+        this.scene.input.keyboard.on("keydown-E",(event)=>{
             this.dash()
         })
 
@@ -62,10 +76,25 @@ export default class Player extends Phaser.Physics.Arcade.Sprite{
 
     }
 
-    dash(x=500){
+    dash(speed=600,time=200,couldown=1000){ //x=vitesse du dash    time=durée du dash   couldown=durée du couldown  //idée: un élément qui affiche le couldown
         if (this.isDashing) return
         
-        this.setAccelerationX(x)
+        this.isDashing = true
+        if (this.direction=="left"){
+            this.setFlipX(true) // Retourne le sprite vers la gauche
+            speed=-speed //on dash vers la gauche
+        }else{
+            this.setFlipX(false) // Retourne le sprite vers la droite
+        }
+        this.setVelocityX(speed)
+        
+        setTimeout(() => {
+            this.setVelocityX(0); // Arrêter le dash après 200ms
+        }, time);
+        
+        setTimeout(() => {
+            this.isDashing=false//couldown de 500 ms avant de laisser le joueur redasher
+        }, time+couldown);
     }
 
 }
