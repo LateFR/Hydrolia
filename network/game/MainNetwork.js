94e59693a0bddcffd9637f1a2260101e4211a6fb
LateFR <rigo.lucas@icloud.com>
2025-02-15 19:23:01 +0100

    Bugs mineurs

--- network/game/MainNetwork.js
+++ network/game/MainNetwork.js
@@ -2,7 +2,7 @@ export default class MainNetwork{
     static ID = "2001"
 
     static get URL_SERVER (){
-        return "http://127.0.0.1:440/"+this.ID+"/" //Constante statique de l'url du serveur. Ne peux pas être modifié
+        return "http://127.0.0.1:440/" //Constante statique de l'url du serveur. Ne peux pas être modifié
     }
 
 }
