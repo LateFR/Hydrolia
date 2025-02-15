c7b7dd8f2a215afb20112b88a66092be96b843b0
LateFR <rigo.lucas@icloud.com>
2025-02-15 17:53:53 +0100

    Bugs mineurs

--- game/world/Chunk.js
+++ game/world/Chunk.js
@@ -1,3 +1,4 @@
+import { World } from "matter"
 import WorldStatic from "./WorldStatic.js"
 export default class Chunk extends Phaser.GameObjects.Container{
     constructor(scene,x,y,bloc_map,player){
@@ -7,12 +8,15 @@ export default class Chunk extends Phaser.GameObjects.Container{
         //Génération du chunk
         this.Statics = new WorldStatic(scene)
         this.blocs = {} //contient la liste de tous les éléments du chunk et leurs positions => ChildElement: (x,y)
-        Object.keys(bloc_map).forEach(element => {  //bloc_map contient les positions et les types de blocs du chunk => (x,y): "type"
+        Object.keys(bloc_map).forEach(element => {  //bloc_map contient les positions et les types de blocs du chunk => (x,y): "type"  (note: x et y sont en "format" hydrolia)
 
             if (bloc_map[element]=="dirt" || bloc_map[element]=="stone") //A modifier. Une fonction de transition type=>asset serait mieux
-                bloc=scene.add.sprite("player") //a changer lorsque les assets seront plus poussé. Actuellement, représent un bloc noir.
+                x = this.Statics.to_phaser_coor(element[0]) //on transforme nos position hydrolia en position in game
+                y = this.Statics.to_phaser_coor(element[1])
+
+                bloc=scene.add.sprite(x,y,"player") //a changer lorsque les assets seront plus poussé. Actuellement, représent un bloc noir.
                 bloc.setSize(this.Statics.bloc_size) //définit la taille du bloc
-                this.blocs[bloc] == (element)
+                this.blocs[bloc] == (x,y)
 
                 if (bloc_map[element]=="dirt"){
                     sprite.setTint(0x6b3f2a)  //Donne une teinte marron au sprite pr le différencier de la stone
