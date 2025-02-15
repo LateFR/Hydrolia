ff8e911a73fc61a7925b971f190d524c05a3c73f
LateFR <rigo.lucas@icloud.com>
2025-02-15 21:33:38 +0100

    Re-ajout de statics.js

---
+++ game/world/Statics.js
@@ -0,0 +1,13 @@
+export default class WorldStatic{
+    constructor(scene){
+        this.scene = scene
+        this.bloc_size = scene.game.config.width/20
+    }
+    
+    to_phaser_coor(x,y){
+        return (x*this.bloc_size,y*this.bloc_size) //fonction pour connaitre les coordonnées phaser a partir de coordonnées hydrolia (bloc par bloc)
+    }
+    to_hydrolia_coor(x,y){
+        return (x/this.bloc_size,y/this.bloc_size)
+    }
+}
